0233094a1adaa42c942fa43bf30e9547dbf5ac88
mystique09 <knightkrypto7@gmail.com>
2022-01-23 22:21:42 +0800

    add gorm.Model

--- models/todo.go
+++ models/todo.go
@@ -1,13 +1,22 @@
 package models
 
+import (
+  "time"
+  "github.com/google/uuid"
+)
+
 type Todo struct {
-  id int32
+  id uuid.UUID `gorm:"primaryKey"json:"_id"`
+  author uuid.UUID `json:"todo_author"`
+  title string `json:"todo_title"`
+  description string `json:"todo_description"`
+  done bool `json:"done"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"created_at"`
+}
+
+type CreateTodo struct {
   title string
   description string
   done bool
-}
-
-type NewTodo struct {
-  title string
-  description string
 }
--- models/user.go
+++ models/user.go
@@ -1,16 +1,28 @@
 package models
 
+import (
+  "gorm.io/gorm"
+  "time"
+  "github.com/google/uuid"
+)
+
 type User struct {
-  Id int32 `json:"_id"`
-  Username string `json:"username"`
+  gorm.Model
+  ID uuid.UUID `gorm:"primaryKey"json:"_id"`
+  Username string `gorm:"unique;not null"json:"username"`
   Password string `json:"password"`
-  Email string `json:"email"`
+  Email string `gorm:"unique;not null"json:"email"`
+  /*CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"created_at"`
+  DeletedAt time.Time `json:"deleted_at"`*/
 }
 
 type QueryableUser struct {
   Id int32 `json:"_id"`
   Username string `json:"username"`
   Email string `json:"email"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"created_at"`
 }
 
 type CreateUser struct {
@@ -21,13 +33,6 @@ type CreateUser struct {
 
 type UserJsonResponse struct {
   Success    bool `json:"success"`
-  Data    []QueryableUser `json:"data"`
+  Data    []User `json:"data"`
   Message string `json:"message"`
-}
-
-func (user *User) VerifyPassword(password string) bool {
-  if user.Password == password {
-    return true
-  }
-  return false
 }
